906215fd9180b82f533c00aa424d133ae586310a
Vyacheslav <rainboss1234@gmail.com>
2023-12-21 21:09:53 +0700

    Add additional options to load resources on the go

--- core/src/com/vych/game/renderer/core/SceneRenderer.java
+++ core/src/com/vych/game/renderer/core/SceneRenderer.java
@@ -2,8 +2,14 @@ package com.vych.game.renderer.core;
 
 import com.badlogic.gdx.graphics.g2d.Batch;
 
+import java.util.List;
+
 public interface SceneRenderer {
     void loadAssets();
+
+    void loadAssets(SceneAsset asset);
+
+    void loadAssets(List<SceneAsset> assetList);
 
     void unloadAssets();
 
--- core/src/com/vych/game/renderer/scenes/GameSceneRenderer.java
+++ core/src/com/vych/game/renderer/scenes/GameSceneRenderer.java
@@ -4,41 +4,39 @@ import com.vych.game.managers.resources.entities.core.ResourceType;
 import com.vych.game.renderer.core.BasicSceneRenderer;
 import com.vych.game.renderer.core.SceneAsset;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class GameSceneRenderer extends BasicSceneRenderer {
-    private final SceneAsset[] assets = {
-            new SceneAsset()
-                    .setResourceName("dropImage")
-                    .setResourceInternalPath("img/droplet.png")
-                    .setResourceType(ResourceType.TEXTURE),
+    public GameSceneRenderer() {
+        super();
+        assets = new ArrayList<>();
+        assets.addAll(List.of(
+                        new SceneAsset()
+                                .setResourceName("dropImage")
+                                .setResourceInternalPath("img/droplet.png")
+                                .setResourceType(ResourceType.TEXTURE),
 
-            new SceneAsset()
-                    .setResourceName("bucketImage")
-                    .setResourceInternalPath("img/bucket.png")
-                    .setResourceType(ResourceType.TEXTURE),
+                        new SceneAsset()
+                                .setResourceName("bucketImage")
+                                .setResourceInternalPath("img/bucket.png")
+                                .setResourceType(ResourceType.TEXTURE),
 
-            new SceneAsset()
-                    .setResourceName("dropSound")
-                    .setResourceInternalPath("snd/drop.wav")
-                    .setResourceType(ResourceType.SOUND),
+                        new SceneAsset()
+                                .setResourceName("dropSound")
+                                .setResourceInternalPath("snd/drop.wav")
+                                .setResourceType(ResourceType.SOUND),
 
-            new SceneAsset()
-                    .setResourceName("rainMusic")
-                    .setResourceInternalPath("snd/rain.mp3")
-                    .setResourceType(ResourceType.MUSIC),
+                        new SceneAsset()
+                                .setResourceName("rainMusic")
+                                .setResourceInternalPath("snd/rain.mp3")
+                                .setResourceType(ResourceType.MUSIC),
 
-            new SceneAsset()
-                    .setResourceName("defaultFont")
-                    .setResourceInternalPath("fonts/font.otf")
-                    .setResourceType(ResourceType.FONT)
-    };
-
-    @Override
-    public void loadAssets() {
-        loadAssets(this.assets);
-    }
-
-    @Override
-    public void unloadAssets() {
-        unloadAssets(this.assets);
+                        new SceneAsset()
+                                .setResourceName("defaultFont")
+                                .setResourceInternalPath("fonts/font.otf")
+                                .setResourceType(ResourceType.FONT)
+                )
+        );
     }
 }
